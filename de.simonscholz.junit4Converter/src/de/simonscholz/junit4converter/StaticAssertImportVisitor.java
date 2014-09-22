d6326e5427daa2ed2a9aa7e69ba52427ea4f4700
Simon Scholz <simon.scholz@vogella.com>
2014-09-22 19:13:07 +0200

    Also added fail method for automatic static imports
    Change-Id: Ic7f2992d9c2de6944c95449c6e0ff832ab91c099
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- de.simonscholz.junit4Converter/src/de/simonscholz/junit4converter/StaticAssertImportVisitor.java
+++ de.simonscholz.junit4Converter/src/de/simonscholz/junit4converter/StaticAssertImportVisitor.java
@@ -7,6 +7,9 @@ import org.eclipse.jdt.core.dom.rewrite.ImportRewrite;
 
 public class StaticAssertImportVisitor extends ASTVisitor {
 
+	private static final String JUNIT_ASSERT_QUALIFIED_NAME = "org.junit.Assert";
+	private static final String ASSERT_FAIL_METHOD = "fail";
+	private static final String ASSERT_METHOD_PREFIX = "assert";
 	private ImportRewrite importRewrite;
 
 	public StaticAssertImportVisitor(ImportRewrite importRewrite) {
@@ -18,8 +21,9 @@ public class StaticAssertImportVisitor extends ASTVisitor {
 	public boolean visit(MethodInvocation node) {
 		SimpleName methodName = node.getName();
 		String methodInvocationName = methodName.getFullyQualifiedName();
-		if (methodInvocationName.startsWith("assert")) {
-			importRewrite.addStaticImport("org.junit.Assert",
+		if (methodInvocationName.startsWith(ASSERT_METHOD_PREFIX)
+				|| methodInvocationName.equals(ASSERT_FAIL_METHOD)) {
+			importRewrite.addStaticImport(JUNIT_ASSERT_QUALIFIED_NAME,
 					methodInvocationName, false);
 		}
 		return true;
